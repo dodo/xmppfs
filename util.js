4794f23baab558d68593ba22ece14d13268cc4f4
dodo <dodo.the.last@gmail.com>
2012-12-11 17:26:52 +0100

    show day specific date prefix in chat log

--- util.js
+++ util.js
@@ -1,9 +1,19 @@
 var moment = require('moment');
+moment.lang('en');
+moment.calendar = {
+    sameDay:  " ",
+    lastDay:  " [Yesterday at] ",
+    nextDay:  " [Tomorrow at] ",
+    lastWeek: " [last] dddd [at] ",
+    nextWeek: " dddd [at] ",
+    sameElse: " DD.MM.YYYY ",
+};
 
 exports.formatDate = function (date) {
-    return "[" + moment(date).format("hh:mm:ss") + "]";
-}
+    date = moment(date);
+    return "[" + date.calendar().substr(1) + date.format("HH:mm:ss") + "]";
+};
 
 exports.escapeResource = function (resource) {
     return ("" + resource).replace("/", "_");
-}
+};
