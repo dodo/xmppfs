25f3a217796efd6ab7864d91d4acee86af67df9b
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2013-10-25 06:58:21 +0200

    add statfs

--- fs.js
+++ fs.js
@@ -438,6 +438,22 @@ function createRouter(root, routes, options) {
             callback();
         },
 
+        statfs: function (callback) {
+            callback(0, {
+                bsize: 1000000,
+                frsize: 1000000,
+                blocks: 1000000,
+                bfree: 1000000,
+                bavail: 1000000,
+                files: 1000000,
+                ffree: 1000000,
+                favail: 1000000,
+                fsid: 1000000,
+                flag: 1000000,
+                namemax: 1000000
+            });
+        },
+
     };
 
     pass.call(-E.ENOTDIR, "readdir");
